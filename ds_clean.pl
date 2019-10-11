566bf199c4960f37df24ec233fade49195716dd3
watanabe.ryuichi <jpz2046@linecorp.com>
2019-10-11 15:03:03 +0900

    first commit

--- ds_clean.pl
+++ ds_clean.pl
@@ -8,15 +8,17 @@ use Data::Dumper;
 use File::Find;
 use File::stat;
 
-sub get_dir {
+sub get_dir_and_unlink {
     my $dir = shift;
     my @list = ();
     my @dot_list = ();
 
+    # Open the passed argument directory.
     opendir(DIR, $dir) or die("Can't open directory: $dir");
     @list = readdir(DIR);
     closedir(DIR);
 
+    # Search the target file by recursively digging the directory.
     foreach my $file (sort @list) {
         next if( $file =~ /^\.{1,2}$/ );
 
@@ -31,6 +33,7 @@ sub get_dir {
             next
         }
     }
+    # Delete the target file.
     foreach(@dot_list){
         unlink $_ or warn "Clound not unlink $_: $!";
     }
@@ -44,7 +47,7 @@ sub main {
         $directory_to_be_deleted = $argv[0];
     }
 
-    get_dir('.');
+    get_dir_and_unlink($directory_to_be_deleted);
 }
 
 main(@ARGV)
